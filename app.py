d85509c41391e5ad3f1b9ef4eb47a7a95e90a395
Vikash Kumar <wiryvikash15@gmail.com>
2025-07-15 02:45:58 +0000

    Frontend Page

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
-from flask import Flask, request, jsonify #request is the property of flask
+from flask import Flask, request, jsonify ,render_template #request is the property of flask
 import pandas as pd
 import pickle
 
 app = Flask(__name__)
 
 # Define the endpoint
+
+def get_cleaned_data():
+    gestation =  form_data['gestation']
+    parity =  form_data['parity']
+    age =  form_data['age']
+    height =  form_data['height']
+    weight =  form_data['weight']
+    smoke =  form_data['smoke']
+    
+    return cleaned_data
+
+@app.route('/',methods=['GET'])
+def home():
+    return render_template("index.html")
+
 @app.route("/predict", methods=['POST'])
 def get_prediction():
     #Get data from the user
-    baby_data = request.get_json()
+    # baby_data = request.get_json()
+    baby_data_form= request.form
+
+    baby_data_cleaned = get_cleaned_data(baby_data_form)
+
 
     #convert into dataframe
-    baby_df =  pd.DataFrame(baby_data)
+    baby_df =  pd.DataFrame(baby_data_cleaned)
 
     #Load the trained machine learning model 
     with open("model/model.pkl",'rb') as obj:
